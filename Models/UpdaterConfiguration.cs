a105595a74569a37e5e72788e2f76ca77b393c74
StylianosBairamis <smpairam@csd.auth.gr>
2026-04-13 12:58:52 +0300

    dropped json configuration file

--- Models/UpdaterConfiguration.cs
+++
@@ -1,7 +0,0 @@
-namespace MobySync.Models;
-
-public class UpdaterConfiguration
-{
-    public GenericSettings GenericSettings { get; set; } = new GenericSettings();
-    public List<ServiceConfiguration> Services { get; set; } = new List<ServiceConfiguration>();
-}
